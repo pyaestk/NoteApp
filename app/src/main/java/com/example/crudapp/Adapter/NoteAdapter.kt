9f1cd38abebc213baa4957ed38a273f19f0c3f3d
dylan <pyaesonethantkyaw206@gmail.com>
2023-10-18 00:52:00 +0630

    second commit
    added adapter and created UI

---
+++ app/src/main/java/com/example/crudapp/Adapter/NoteAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.crudapp.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.crudapp.Model.Note
+import com.example.crudapp.R
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    var notes: List<Note> = ArrayList()
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title: TextView = itemView.findViewById(R.id.titleTextView)
+        val description: TextView = itemView.findViewById(R.id.desTextView)
+        val cardView: CardView = itemView.findViewById(R.id.cardView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.note_list_item, parent, false)
+
+        return NoteViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        var currentNote: Note = notes[position]
+        holder.title.text = currentNote.title
+        holder.description.text = currentNote.des
+    }
+
+    //for live data
+    fun setNote(myNotes: List<Note>) {
+        this.notes = myNotes
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/crudapp/View/MainActivity.kt
+++ app/src/main/java/com/example/crudapp/View/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.crudapp.Adapter.NoteAdapter
 import com.example.crudapp.NoteApplication
 import com.example.crudapp.R
 import com.example.crudapp.Repository.NoteRepository
@@ -13,10 +16,18 @@ import com.example.crudapp.ViewModel.NoteViewModelFactory
 class MainActivity : AppCompatActivity() {
 
     lateinit var noteViewModel: NoteViewModel
+    lateinit var recyclerView: RecyclerView
+    lateinit var noteAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.noteRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        
+        noteAdapter = NoteAdapter()
+        recyclerView.adapter = noteAdapter
 
         val viewModelFactory = NoteViewModelFactory((application as NoteApplication).repository)
 
@@ -25,8 +36,8 @@ class MainActivity : AppCompatActivity() {
         //update UI
         noteViewModel.myAllNotes.observe(this, Observer { notes ->
 
+            noteAdapter.setNote(notes)
             
-
         }) //live data method
 
     }
